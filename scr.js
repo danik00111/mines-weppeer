90e0dd78a9878fa61cddaeac37a60a7d1edff5d5
danik00111 <115283667+danik00111@users.noreply.github.com>
2024-01-15 18:38:13 +0200

    (almost) complete rollback to af10256 "fix"

--- scr.js
+++ scr.js
@@ -15,42 +15,12 @@ const neighbourlib = [
   {x: 1,y:-1},
   {x: 0,y:-1},
 ];
-const zergnumbor = (x,y) => {
-  let count = 0;
-  for(let i=0;i<neighbourlib.length;i++)try{
-    if(isMine[y+neighbourlib[i].y][x+neighbourlib[i].x])count++
-    if(getCell(x+neighbourlib[i].x,y+neighbourlib[i].y).classList.contains("flag")) count--;
-  }catch(e){/*ignore error*/}
-  return count;
-}
 const numbor = (x,y) => {
   let count = 0;
   for(let i=0;i<neighbourlib.length;i++)try{
     if(isMine[y+neighbourlib[i].y][x+neighbourlib[i].x])count++
   }catch(e){/*ignore error*/}
   return count;
-}
-const reRender = () => {
-  let x; let y; let count;
-  [...document.querySelectorAll('cell[n]')].forEach(e=>{
-    x = parseInt(e.getAttribute("pos"));
-    y = parseInt(e.parentElement.getAttribute("pos"));
-    if(document.getElementById('toggle-zerg').classList.contains('glow')) {
-      count = numbor(x,y);
-      if(e.getAttribute("n")=='') {e.setAttribute("vi-n",'')} else {
-        for(let i=0;i<neighbourlib.length;i++)
-          try{
-            if(getCell(x+neighbourlib[i].x,y+neighbourlib[i].y).classList.contains('flag'))count--
-          }catch(er){};
-        e.setAttribute("vi-n",count);
-      }
-    } else e.setAttribute("vi-n",(numbor(x,y)||''));
-  })
-}
-const flagReEval = () => {
-  if(gamestate != 'on') return;
-  document.getElementById('minecount').innerHTML = minespots.length - [...document.querySelectorAll('cell.flag')].length;
-  if(document.getElementById('minecount').innerHTML=='0') document.getElementById('quickend').classList.add('shown');
 }
 let gamestate = 'waiting';
 let isMine;
@@ -60,18 +30,9 @@ const flag_ = (x,y) => {
   if(getCell(x,y).getAttribute("n")!==null){open_(x,y,'real click');return} /* shorthand to instead goto a chord check, allows chording with rbm */
   getCell(x,y).classList.toggle('flag');
   document.getElementById('minecount').innerHTML =
-    parseInt(document.getElementById('minecount').innerHTML) +
-    (getCell(x,y).classList.contains('flag') ? -1 : 1);
+  parseInt(document.getElementById('minecount').innerHTML) +
+  (getCell(x,y).classList.contains('flag') ? -1 : 1)
   if(document.getElementById('minecount').innerHTML=='0') document.getElementById('quickend').classList.add('shown');
-  if(document.getElementById('toggle-zerg').classList.contains('glow')) {
-    for(let i=0;i<neighbourlib.length;i++) {
-      try{
-        if(getCell(x+neighbourlib[i].x,y+neighbourlib[i].y).getAttribute("n")!==null){
-          getCell(x+neighbourlib[i].x,y+neighbourlib[i].y).setAttribute("vi-n",zergnumbor(x+neighbourlib[i].x,y+neighbourlib[i].y));
-        }
-      }catch(e){}
-    }
-  }
 }
 const open_ = (x,y,c) => {
   if(x<0||y<0||x>width||y>height)return;
@@ -92,7 +53,6 @@ const open_ = (x,y,c) => {
         if(flagcount[i]&&getCell(x+neighbourlib[i].x,y+neighbourlib[i].y).getAttribute("n")===null)open_(x+neighbourlib[i].x,y+neighbourlib[i].y,'chording')
       }
     }
-    flagReEval(); // just in case, to stop a rare bug
   }
   //^ khord if the cell has enough flags around it
   if(gamestate=='waiting') gameStart(
@@ -109,6 +69,8 @@ const open_ = (x,y,c) => {
     gamestate = 'kaboom';
     getCell(x,y).classList.add('explo');
     for(let i=0;i<minespots.length;i++)getCell(minespots[i].x,minespots[i].y).classList.add('mine');
+    minespots = [];
+    isMine = undefined;
     document.querySelector('input[name="difficulty"]:checked').checked = false;
     realtime = timeEnd-timeStart;
     document.getElementById('timer').innerHTML = Math.floor(realtime / 1000);
@@ -117,12 +79,9 @@ const open_ = (x,y,c) => {
     return;
   };
   //^ blow the player's house up if they click a mine
-  let iszerg = document.getElementById('toggle-zerg').classList.contains('glow');
   if(numbor(x,y)>0){
-    getCell(x,y).setAttribute("vi-n",iszerg ? zergnumbor(x,y) : numbor(x,y));
-    getCell(x,y).setAttribute("n",numbor(x,y));
+    getCell(x,y).setAttribute("n",numbor(x,y)); // i couldve just done a `count || ''`, but i need to stuff more logic inside this
   } else {
-    getCell(x,y).setAttribute("vi-n",'');
     getCell(x,y).setAttribute("n",'');
     const queue=[{x,y}];
     while(queue.length){
@@ -132,12 +91,9 @@ const open_ = (x,y,c) => {
         const nY=cY+neighbourlib[i].y;
         if((nX>=0)&&(nY>=0)&&(nX<width)&&(nY<height))if((getCell(nX,nY).getAttribute('n')===null)){
           const nC=numbor(nX,nY);
-          const nZC=zergnumbor(nX,nY);
           if(nC>0){
-            getCell(nX,nY).setAttribute('vi-n',iszerg ? nZC.toString() : nC.toString());
             getCell(nX,nY).setAttribute('n',nC.toString())
           }else{
-            getCell(nX,nY).setAttribute('vi-n','');
             getCell(nX,nY).setAttribute('n','');
             queue.push({"x":nX,"y":nY});
           }
@@ -145,7 +101,6 @@ const open_ = (x,y,c) => {
         try{getCell(nX,nY).classList.remove('flag')}catch(e){/*ignore and opt out*/};
       }
     }
-    flagReEval(); // just in case, to stop a rare bug
   }
   //^ display the numbor, and if it's a 0, trigger a nuclear chain reaction
   if([...document.querySelectorAll('cell:not([n])')].length == minespots.length) {
@@ -197,7 +152,7 @@ const gameStart = (minecount,firstClickX,firstClickY) => {
 let width; let height;
 const makeboard = (w,h,m) => {
   width = w; height = h;
-  gamestate = 'waiting'; time = 0; clearInterval(timer); document.getElementById('timer').innerHTML = 0;
+  gamestate = 'waiting'; isMine = undefined; time = 0; clearInterval(timer); document.getElementById('timer').innerHTML = 0;
   document.getElementById('decimal').classList.remove('shown');
   w = parseInt(w); h = parseInt(h); m = parseInt(m);
   if((w<3)||(h<3)||((m+10)>(w*h))||(m<2)||(isNaN(w))||(isNaN(h))||(isNaN(m))){
